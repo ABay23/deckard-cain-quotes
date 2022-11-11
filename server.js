d53781b0b4f62c9c92c5d06f721aeadd186fb528
Alejandro Bay <alejandrobay23@gmail.com>
2022-11-10 20:47:06 -0500

    This is CRU out of CRUD working as intended

--- server.js
+++ server.js
@@ -4,40 +4,79 @@ const bodyParser = require('body-parser')
 const MongoClient = require('mongodb').MongoClient
 const app = express()
 const PORT = 4000
+const dbName = 'deckard-cain-quotes'
+var db, collection
 
+app.use(bodyParser.urlencoded({ extended: true }))
 app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(bodyParser.json())
 
 dotenv.config({ path: './config/config.env' })
 
-const connectDB = async () => {
-  const conn = await MongoClient.connect(
-    process.env.MONGO_URI,
-    { useUnifiedTopology: true },
-    (err, client) => {
-      if (err) return console.error(err)
-      console.log('Connected to DB...')
-      const db = client.db('deckard-cain-quotes')
-      const quotesCollection = db.collection('quotes')
-      app.get('/', (req, res) => {
-        res.sendFile(__dirname + '/index.html')
-      })
-
-      app.post('/quotes', (req, res) => {
-        quotesCollection
-          .insertOne(req.body)
-          .then((result) => {
-            res.redirect(307, '/')
-          })
-          .catch((error) => console.error(error))
-      })
+MongoClient.connect(
+  process.env.MONGO_URI,
+  { useUnifiedTopology: true },
+  (err, client) => {
+    if (err) {
+      return console.error(err)
+    } else {
+      db = client.db(dbName)
+      console.log(`Connected to DB ${dbName}...`)
     }
-  )
-}
-connectDB()
+  }
+)
 
-app.use(bodyParser.urlencoded({ extended: true }))
+// app.get('/', (req, res) => {
+//   res.sendFile(__dirname + '/index.html')
+// })
 
-app.listen(PORT, (req, res) => {
+app.post('/quotes', (req, res) => {
+  db.collection('quotes')
+    .insertOne(req.body)
+    .then((result) => {
+      // if (err) {
+      //   return console.log(err)
+      // } else {
+      console.log('saved')
+      res.redirect('/')
+      // }
+    })
+
+    .catch((error) => console.error(error))
+})
+
+app.get('/', (req, res) => {
+  db.collection('quotes')
+    .find()
+    .toArray()
+    .then((result) => {
+      res.render('index.ejs', { quotes: result })
+    })
+
+    .catch((error) => console.error(error))
+})
+
+app.put('/quotes', (req, res) => {
+  db.collection('quotes')
+    .findOneAndUpdate(
+      { name: 'Deckard' },
+      {
+        $set: {
+          name: req.body.name,
+          quote: req.body.quote,
+        },
+      },
+      {
+        upsert: true,
+      }
+    )
+    .then((result) => {
+      res.json('success')
+    })
+    .catch((error) => console.error(error))
+})
+
+app.listen(process.env.PORT || PORT, (req, res) => {
   console.log(`Stay awhile and listen to the  server ${PORT}! ...`)
-  console.log('Stay awhile and listen... Server is running as intended...')
 })
